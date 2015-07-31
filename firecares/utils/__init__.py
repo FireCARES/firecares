a4ff20af7f5847e6113e399815b975434f75cbfb
Tyler Garner <garnertb@gmail.com>
2015-07-31 13:05:00 -0400

    Set thumbnails to jpg vs pngs.

--- firecares/utils/__init__.py
+++ firecares/utils/__init__.py
@@ -1,1 +1,15 @@
+from PIL import Image
 
+
+def convert_png_to_jpg(img):
+    """
+    Converts a png to a jpg.
+    :param img: Absolute path to the image.
+    :returns: the filename
+    """
+    im = Image.open(img)
+    bg = Image.new("RGB", im.size, (255, 255, 255))
+    bg.paste(im, im)
+    filename = img.replace('png', 'jpg')
+    bg.save(filename, quality=85)
+    return filename
