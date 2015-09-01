85cb4d94e961257f94dcd6b79bd51cac56c4e900
Joe Meilinger <joemeilinger@gmail.com>
2015-08-31 21:07:44 -0500

    Added password reset / forgot username workflows
    fixes #13

---
+++ firecares/firecares_core/forms.py
@@ -0,0 +1,61 @@
+from __future__ import unicode_literals
+
+from collections import OrderedDict
+
+from django import forms
+from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.hashers import (
+    UNUSABLE_PASSWORD_PREFIX, identify_hasher,
+)
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMultiAlternatives
+from django.forms.utils import flatatt
+from django.template import loader
+from django.utils.encoding import force_bytes
+from django.contrib.auth.forms import PasswordResetForm
+from django.utils.html import format_html, format_html_join
+from django.utils.http import urlsafe_base64_encode
+from django.utils.safestring import mark_safe
+from django.utils.text import capfirst
+from django.utils.translation import ugettext, ugettext_lazy as _
+from firecares.tasks.email import send_mail
+
+class ForgotUsernameForm(forms.Form):
+    email = forms.EmailField()
+
+    def send_mail(self, subject, email_template_name, context, from_email,
+                  to_email, html_email_template_name=None):
+        """
+        Sends a django.core.mail.EmailMultiAlternatives to `to_email`.
+        """
+
+        body = loader.render_to_string(email_template_name, context)
+
+        email_message = EmailMultiAlternatives(subject, body, from_email, [to_email])
+        if html_email_template_name is not None:
+            html_email = loader.render_to_string(html_email_template_name, context)
+            email_message.attach_alternative(html_email, 'text/html')
+
+        send_mail.delay(email_message)
+
+
+class FirecaresPasswordResetForm(PasswordResetForm):
+
+    def send_mail(self, subject_template_name, email_template_name,
+                  context, from_email, to_email, html_email_template_name=None):
+        """
+        Sends a django.core.mail.EmailMultiAlternatives to `to_email`.
+        """
+        subject = loader.render_to_string(subject_template_name, context)
+        # Email subject *must not* contain newlines
+        subject = ''.join(subject.splitlines())
+        body = loader.render_to_string(email_template_name, context)
+
+        email_message = EmailMultiAlternatives(subject, body, from_email, [to_email])
+        if html_email_template_name is not None:
+            html_email = loader.render_to_string(html_email_template_name, context)
+            email_message.attach_alternative(html_email, 'text/html')
+
+        send_mail.delay(email_message)
