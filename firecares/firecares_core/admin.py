f0bef3f32a4677b00992792a10cc267448451dbc
Joe Meilinger <joemeilinger@gmail.com>
2016-03-14 22:09:07 -0500

    Updated to use local openlayers vs remote in administration
    fixes #105

--- firecares/firecares_core/admin.py
+++ firecares/firecares_core/admin.py
@@ -1,14 +1,19 @@
 from .models import Address, ContactRequest
+from django.conf import settings
 from django.contrib.gis import admin
 
 
-class AddressAdmin(admin.OSMGeoAdmin):
+class LocalOpenLayersAdmin(admin.OSMGeoAdmin):
+    openlayers_url = settings.STATIC_URL + 'openlayers/OpenLayers.js'
+
+
+class AddressAdmin(LocalOpenLayersAdmin):
     list_display = ['__unicode__']
     list_filter = ['state_province']
     search_fields = ['address_line1', 'state_province', 'city']
 
 
-class ContactRequestAdmin(admin.OSMGeoAdmin):
+class ContactRequestAdmin(LocalOpenLayersAdmin):
     list_display = ['name', 'email', 'created_at']
     search_fields = ['name', 'email']
 
--- firecares/usgs/admin.py
+++ firecares/usgs/admin.py
@@ -1,30 +1,31 @@
 from .models import GovUnits, Reserve, NativeAmericanArea, IncorporatedPlace, UnincorporatedPlace, CountyorEquivalent, \
     MinorCivilDivision, StateorTerritoryHigh
+from firecares.firecares_core.admin import LocalOpenLayersAdmin
 from django.contrib.gis import admin
 
 
-class GovUnitsAdmin(admin.OSMGeoAdmin):
+class GovUnitsAdmin(LocalOpenLayersAdmin):
     pass
 
 
-class ReserveAdmin(admin.OSMGeoAdmin):
+class ReserveAdmin(LocalOpenLayersAdmin):
     search_fields = ['name']
     list_display = ['name', 'fcode', 'admintype']
 
 
-class IncorporatedPlaceAdmin(admin.OSMGeoAdmin):
+class IncorporatedPlaceAdmin(LocalOpenLayersAdmin):
     search_fields = ['place_name', 'state_name', 'place_fipscode']
-    list_display = [ 'place_name', 'state_name', 'fcode', 'place_fipscode']
-    list_filter = [ 'state_name', 'fcode']
+    list_display = ['place_name', 'state_name', 'fcode', 'place_fipscode']
+    list_filter = ['state_name', 'fcode']
 
 
-class CountyOrEquivalentAdmin(admin.OSMGeoAdmin):
+class CountyOrEquivalentAdmin(LocalOpenLayersAdmin):
     list_display = ['county_name', 'state_name', 'fcode', 'stco_fipscode']
     list_filter = ['state_name', 'fcode']
     search_fields = ['county_name', 'state_name', 'stco_fipscode']
 
 
-class MinorCivilDivisionAdmin(admin.OSMGeoAdmin):
+class MinorCivilDivisionAdmin(LocalOpenLayersAdmin):
     list_display = ['minorcivildivision_name', 'state_name', 'fcode']
     list_filter = ['state_name', 'fcode']
     search_fields = ['minorcivildivision_name', 'state_name']
@@ -37,4 +38,3 @@ admin.site.register(MinorCivilDivision, MinorCivilDivisionAdmin)
 
 for model in [GovUnits, NativeAmericanArea, StateorTerritoryHigh]:
     admin.site.register(model, GovUnitsAdmin)
-
