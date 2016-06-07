c8869e4631fc8f6b6801cc065aca012e0cf5c47e
Tyler Garner <garnertb@gmail.com>
2016-06-07 13:50:47 -0400

    Add the export-building-fires management command.

---
+++ firecares/firestation/management/commands/export-building-fires.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from firecares.firestation.models import FireDepartment
+
+
+class Command(BaseCommand):
+    """
+    This command is used to export data that department heat maps visualize.
+    """
+
+    help = 'Creates a sql file to export building fires from.'
+
+    def handle(self, *args, **options):
+        vals = FireDepartment.objects.filter(fdid__isnull=False, state__isnull=False).exclude(fdid__exact='')
+
+        sql = """\COPY (select alarm, a.inc_type, alarms,ff_death, oth_death, ST_X(geom) as x, st_y(geom) as y  from buildingfires a left join incidentaddress b using (state, inc_date, exp_no, fdid, inc_no) where state='{state}' and fdid='{fdid}') to '/tmp/data/{id}-building-fires.csv' DELIMITER ',' CSV HEADER;"""
+
+        for fd in vals:
+            self.stdout.write(sql.format(fdid=fd.fdid, state=fd.state, id=fd.id) + '\n')
