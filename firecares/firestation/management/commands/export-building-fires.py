9a9ab21b66991171fc7b6288d9c734dc05d82a3d
Tyler Garner <garnertb@gmail.com>
2016-08-01 09:18:10 -0400

    Update export building fires command.

--- firecares/firestation/management/commands/export-building-fires.py
+++ firecares/firestation/management/commands/export-building-fires.py
@@ -12,7 +12,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         vals = FireDepartment.objects.filter(fdid__isnull=False, state__isnull=False).exclude(fdid__exact='')
 
-        sql = """\COPY (select alarm, a.inc_type, alarms,ff_death, oth_death, ST_X(geom) as x, st_y(geom) as y  from buildingfires a left join incidentaddress b using (state, inc_date, exp_no, fdid, inc_no) where state='{state}' and fdid='{fdid}') to '/tmp/data/{id}-building-fires.csv' DELIMITER ',' CSV HEADER;"""
+        sql = """
+       \COPY (select alarm, a.inc_type, alarms,ff_death, oth_death, ST_X(geom) as x, st_y(geom) as y  from buildingfires a left join incidentaddress b using (state, inc_date, exp_no, fdid, inc_no) where state='{state}' and fdid='{fdid}') to PROGRAM 'aws s3 cp - s3://firecares-pipeline/heatmaps/{id}-building-fires.csv --acl=\"public-read\"' DELIMITER ',' CSV HEADER;
+        """
 
         for fd in vals:
             self.stdout.write(sql.format(fdid=fd.fdid, state=fd.state, id=fd.id) + '\n')
