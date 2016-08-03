314995f4c45efb9dbc1c6e4851cf193495db445f
Lou Hayt <louhayt@gmail.com>
2016-08-03 23:18:06 +0300

    added the favorite button on department cards and station list items

--- firecares/firestation/static/firestation/js/services/favoriteService.js
+++ firecares/firestation/static/firestation/js/services/favoriteService.js
@@ -17,7 +17,7 @@
     FavoriteController.$inject = ['$scope', 'favorite'];
     function FavoriteController($scope, favorite) {
         $scope.onFavorite = favorite.onFavorite;
-        $scope.setFavoriteButtonColorSelected = favorite.setFavoriteButtonColorSelected;
+        $scope.initFavoriteButtonColor = favorite.initFavoriteButtonColor;
     }
 
     // The service sends a POST request to favit and manages the button HTML elements
@@ -25,14 +25,14 @@
     function FavoriteService($http) {
 
         var selected_color = '#FEBE00'
-        var unselected_color = ''
-        var icon_id = 'favorite-star'
+        var unselected_color = 'silver' // TEST this should be empty string
+        var icon_id = 'favorite-star-' // + id (id prevents multiple favorite buttons on the same page from colliding)
 
         var disabled = []; // object ids that are currently being processed (TODO maybe its better to store this flag on the button element somehow?)
 
-        this.setFavoriteButtonColorSelected = function () {
-            var star = angular.element(document.getElementById(icon_id));
-            star.css('color', selected_color);
+        this.initFavoriteButtonColor = function (id, selected) {
+            var star = angular.element(document.getElementById(icon_id+id));
+            star.css('color', selected == 'True' ? selected_color : unselected_color);
         };
 
         this.onFavorite = function (model, id) {
@@ -40,7 +40,7 @@
                 return;
             //console.log("Favorite an object for the current user, object id:" + id + " object model: " + model);
             disabled.push(id); // disable further requests for this object
-            var star = angular.element(document.getElementById(icon_id));
+            var star = angular.element(document.getElementById(icon_id+id));
             // flip the visual selection indication of the button immediately so the input feels responsive
             star.css('color', star.css('color') == selected_color ? unselected_color : selected_color);
             $http({
