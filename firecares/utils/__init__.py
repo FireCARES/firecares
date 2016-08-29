bf007267246bd317dc3ccad9f5cf8a9f452b3e0b
Tyler Garner <garnertb@gmail.com>
2016-08-29 10:43:14 -0400

    Remove the unused convert_png_to_jpg method.

--- firecares/utils/__init__.py
+++ firecares/utils/__init__.py
@@ -3,20 +3,6 @@ from storages.backends.s3boto import S3BotoStorage
 from PIL import Image
 
 
-def convert_png_to_jpg(img):
-    """
-    Converts a png to a jpg.
-    :param img: Absolute path to the image.
-    :returns: the filename
-    """
-    im = Image.open(img)
-    bg = Image.new("RGB", im.size, (255, 255, 255))
-    bg.paste(im, im)
-    filename = img.replace('png', 'jpg')
-    bg.save(filename, quality=85)
-    return filename
-
-
 class CachedS3BotoStorage(S3BotoStorage):
     """
     S3 storage backend that saves the files locally, too.
