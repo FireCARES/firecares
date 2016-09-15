d24b0b3c935ffb49c8ce48d06cc159c111df4d79
Tyler Garner <garnertb@gmail.com>
2016-09-15 12:56:20 -0400

    Add parcel data to the department detail map.

--- firecares/firestation/static/firestation/js/controllers/departmentDetail.js
+++ firecares/firestation/static/firestation/js/controllers/departmentDetail.js
@@ -5,9 +5,9 @@
         .controller('jurisdictionController', JurisdictionController)
     ;
 
-    JurisdictionController.$inject = ['$scope', '$timeout', '$http', 'FireStation', 'map', 'heatmap'];
+    JurisdictionController.$inject = ['$scope', '$timeout', '$http', 'FireStation', 'map', 'heatmap', '$filter'];
 
-    function JurisdictionController($scope, $timeout, $http, FireStation, map, heatmap) {
+    function JurisdictionController($scope, $timeout, $http, FireStation, map, heatmap, $filter) {
         var departmentMap = map.initMap('map', {scrollWheelZoom: false});
         var showStations = true;
         var stationIcon = L.FireCARESMarkers.firestationmarker();
@@ -69,7 +69,6 @@
         //
         // Heatmap
         //
-
         var heatmapDataUrl = 'https://s3.amazonaws.com/firecares-test/' + config.id + '-building-fires.csv';
         $http.head(heatmapDataUrl)
             .then(function(response) {
@@ -119,7 +118,140 @@
                         $scope.showHeatmapCharts = show;
                     });
                 }
-            })
-        ;
+            });
+
+        //
+        // Parcels
+        //
+        var parcels = new L.TileLayer.MVTSource({
+          url: "https://{s}.firecares.org/parcels/{z}/{x}/{y}.pbf",
+          debug: false,
+          clickableLayers: null,
+          mutexToggle: true,
+          maxZoom: 18,
+          minZoom: 15,
+
+          getIDForLayerFeature: function(feature) {
+            return feature.properties.parcel_id;
+          },
+
+          filter: function(feature, context) {
+            return true;
+          },
+
+
+          style: function(feature) {
+            var style = {};
+            var selected = style.selected = {};
+            var pointRadius = 1;
+
+            function ScaleDependentPointRadius(zoom) {
+              //Set point radius based on zoom
+              var pointRadius = 1;
+              if (zoom >= 0 && zoom <= 7) {
+                pointRadius = 1;
+              }
+              else if (zoom > 7 && zoom <= 10) {
+                pointRadius = 2;
+              }
+              else if (zoom > 10) {
+                pointRadius = 3;
+              }
+
+              return pointRadius;
+            }
+
+            var type = feature.type;
+            switch (type) {
+              case 1: //'Point'
+                // unselected
+                style.color = CICO_LAYERS[feature.properties.type].color || '#3086AB';
+                style.radius = ScaleDependentPointRadius;
+                // selected
+                style.selected = {
+                  color: 'rgba(255,255,0,0.5)',
+                  radius: 6
+                };
+                break;
+              case 2: //'LineString'
+                // unselected
+                style.color = 'rgba(161,217,155,0.8)';
+                style.size = 3;
+                // selected
+                style.selected = {
+                  color: 'rgba(255,255,0,0.5)',
+                  size: 6
+                };
+                break;
+              case 3: //'Polygon'
+                // unselected
+                style.color = 'rgba(149,139,255,0)';
+                style.outline = {
+                  color: 'rgb(20,20,20)',
+                  size: 1
+                };
+                // selected
+                style.selected = {
+                  color: 'rgba(255,255,0,0.5)',
+                  outline: {
+                    color: '#d9534f',
+                    size: 3
+                  }
+                };
+
+            }
+
+            return style;
+          },
+
+          onClick: function(evt) {
+            var message = 'No parcel data found at this location.';
+            if (evt.feature != null) {
+                message = '';
+
+                var items = {
+                    'Address': 'addr',
+                    'City': 'city',
+                    'State': 'state',
+                    'Zip': 'zip',
+                    'Building Sq Footage': 'bld_sq_ft',
+                    'Stories': 'story_nbr',
+                    'Units': 'units_nbr',
+                    'Condition': 'condition',
+                    'Year Built': 'yr_blt',
+                    'Rooms': 'rooms',
+                    'Bed Rooms': 'bed_rooms',
+                    'Total Value': 'tot_val',
+                    'Land Value': 'lan_val',
+                    'Improvements Value': 'imp_val'
+                };
+
+                _.each(_.pairs(items), function(pair){
+                    var key = pair[0];
+                    var value = evt.feature.properties[pair[1]];
+
+                    if (key.indexOf('Value') !== -1 && value != null) {
+                        value = $filter('currency')(value, '$', 0);
+                    }
+
+                    if ((key === 'Building Sq Footage' || key === 'Units') && value != null) {
+                        value = $filter('number')(value, 0);
+                    }
+
+                    value = value ? value : 'Unknown';
+                    message += '<b>' + key + ':</b> ' + value + '</br>';
+                });
+
+                L.popup()
+                    .setLatLng(evt.latlng)
+                    .setContent(message)
+                    .openOn(departmentMap);
+
+            }
+          }
+
+        });
+        layersControl.addOverlay(parcels, 'Parcels');
+
     }
 })();
