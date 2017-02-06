a21c99f013f42d74a92d5965e1cc1938ab78b378
Tyler Garner <garnertb@gmail.com>
2017-02-06 08:21:30 -0500

    Remove statsd.

--- firecares/settings/production.py
+++ firecares/settings/production.py
@@ -2,29 +2,6 @@ import os
 from firecares.settings.base import *  # noqa
 from celery.schedules import crontab
 
-INSTALLED_APPS = (
-    'django_statsd',
-) + INSTALLED_APPS  # noqa
-
-STATSD_HOST = 'stats.garnertb.com'
-STATSD_PREFIX = 'firecares'
-STATSD_CELERY_SIGNALS = True
-
-STATSD_PATCHES = [
-    'django_statsd.patches.db',
-    'django_statsd.patches.cache',
-]
-
-MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django_statsd.middleware.TastyPieRequestTimingMiddleware'
-) + MIDDLEWARE_CLASSES  # noqa
-
-STATSD_PATCHES = [
-    'django_statsd.patches.db',
-    'django_statsd.patches.cache',
-]
 
 CACHES = {
     'default': {
