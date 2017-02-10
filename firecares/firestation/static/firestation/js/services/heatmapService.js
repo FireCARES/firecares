febc7b705ac563b805ae6ef612074777c6e8e9a9
Joe Meilinger <joemeilinger@gmail.com>
2017-02-10 13:28:08 -0600

    Use "Unknown" instead of "N/A" for heatmap parcel hazard level bucket

--- firecares/firestation/static/firestation/js/services/heatmapService.js
+++ firecares/firestation/static/firestation/js/services/heatmapService.js
@@ -34,7 +34,7 @@
             months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
             daysOfWeek: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
             hours: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'],
-            risk: ['N/A', 'Low', 'Medium', 'High'],
+            risk: ['Unknown', 'Low', 'Medium', 'High'],
         };
 
         function processData(allText) {
