08f15638220834a7c9323d2abf3ca28ce8662c2e
Joe Meilinger <joemeilinger@gmail.com>
2017-02-22 10:39:39 -0600

    Add score validation notebook

---
+++ firecares/firestation/migrations/0047_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('firestation', '0046_firedepartmentriskmodels_dist_model_score_fire_count'),
+        ('firestation', '0046_datafeedback'),
+    ]
+
+    operations = [
+    ]
