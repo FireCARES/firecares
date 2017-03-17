08e3e487bebb1f7bca1ef8abb71682f3be404990
Hunter Connelly <hunterconnelly@hunterconnelly.com>
2017-03-17 14:16:04 -0400

    Add text/html versions to all emails.

--- firecares/firecares_core/forms.py
+++ firecares/firecares_core/forms.py
@@ -18,8 +18,10 @@ class ForgotUsernameForm(forms.Form):
         """
 
         body = loader.render_to_string(email_template_name, context)
+        body_html = loader.render_to_string(html_email_template_name, context)
 
         email_message = EmailMultiAlternatives(subject, body, from_email, [to_email])
+        email_message.attach_alternative(body_html, "text/html")
         if html_email_template_name is not None:
             html_email = loader.render_to_string(html_email_template_name, context)
             email_message.attach_alternative(html_email, 'text/html')
