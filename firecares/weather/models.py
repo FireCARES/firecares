763336fbad4de04429b1af315c2891e7230d0b21
ssontag55 <sontag.stephen@gmail.com>
2017-10-12 17:29:31 -0400

    change back to unique firedepart id

--- firecares/weather/models.py
+++ firecares/weather/models.py
@@ -169,9 +169,9 @@ class WeatherWarnings(models.Model):
                 print("Warning Expires " + str(weatherWarnings.expiration.strftime('%c')))
 
                 #  Check if warning is already loaded and update as needed
-                if DepartmentWarnings.objects.filter(warningname=weatherWarnings.warnid, departmentfdid=fireDept.fdid):
+                if DepartmentWarnings.objects.filter(warningname=weatherWarnings.warnid, departmentfdid=fireDept.id):
 
-                    dataduplicate = DepartmentWarnings.objects.filter(warningname=weatherWarnings.warnid, departmentfdid=fireDept.fdid)
+                    dataduplicate = DepartmentWarnings.objects.filter(warningname=weatherWarnings.warnid, departmentfdid=fireDept.id)
                     deptupdate = dataduplicate[0]
                     deptupdate.warningname = weatherWarnings.warnid
                     deptupdate.prod_type = weatherWarnings.prod_type
@@ -188,7 +188,7 @@ class WeatherWarnings(models.Model):
 
                     dataadd = {}
                     dataadd['department'] = fireDept
-                    dataadd['departmentfdid'] = fireDept.fdid
+                    dataadd['departmentfdid'] = fireDept.id
                     dataadd['departmentname'] = fireDept.name
                     dataadd['warningfdid'] = weatherWarnings
                     dataadd['warningname'] = weatherWarnings.warnid
