d548f447c91ec3d10155ee635603a5e655e72cc7
ssontag55 <sontag.stephen@gmail.com>
2018-01-16 16:37:12 -0500

    merge db firedepartment conflicts

---
+++ firecares/firestation/migrations/0060_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('firestation', '0059_auto_20171221_1633'),
+        ('firestation', '0053_auto_20171214_2119'),
+    ]
+
+    operations = [
+    ]
