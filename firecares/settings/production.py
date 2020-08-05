0d4e43ae387e00f9640f2a1db19ac4b75b6aa8cf
Jose Oliveros <jose.oliveros.1983@gmail.com>
2020-08-05 12:18:16 -0500

    add allowed_hosts
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- firecares/settings/production.py
+++ firecares/settings/production.py
@@ -2,7 +2,7 @@ import os
 from firecares.settings.base import *  # noqa
 from celery.schedules import crontab
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN', None)
 AWS_S3_URL_PROTOCOL = os.getenv('AWS_S3_URL_PROTOCOL', 'http:')
