642041f6973ab6084851518f4826c55213433de3
Ben Coleman <ben.coleman@prominentedge.com>
2021-04-09 16:15:21 -0700

    Use the right print statement

--- firecares/celery.py
+++ firecares/celery.py
@@ -48,7 +48,7 @@ def task_exists(name, args=None, kwargs=None):
     from celery.task.control import inspect
     inspector = inspect()
 
-    print(inspector.app.backend.url)
+    print 'inspector.app.backend.url: {}'.format(inspector.app.backend.url)
 
     def matches(task):
         if name not in task['name']:
